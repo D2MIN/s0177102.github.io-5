6b420070e362d69666a3735d42630a41be977345
D2MIN <aamor8448@gmail.com>
2023-10-11 01:20:33 +0300

    Final Changes

--- skript.js
+++ skript.js
@@ -2,34 +2,68 @@ var inp_coll = document.getElementById("inp_coll");
 var prod = document.getElementById("select_prod")
 var btn = document.getElementById("btn_sub");
 var img = document.getElementById("img_prod")
+var cell = document.getElementById("text_cell");
+var cell_text = document.getElementById("cell_text");
+
+var add = document.getElementById("add");
+var clear = document.getElementById("clear");
+let xcell = 25;
+var sum = 0;
 
 prod.addEventListener("change", function() {
     var product = prod.value;
     if(product === "Банан"){
         img.src = "img/banan.jpg";
+        cell.textContent = "49 рублей";
+        xcell = 49;
     }else if(product === "Клубника"){
         img.src = "img/clubnika.jpg";
+        cell.textContent = "28 рублей";
+        xcell = 28;
     }else if(product === "Вишня"){
         img.src = "img/vishnya.jpg";
+        cell.textContent = "32 рублей";
+        xcell = 32;
     }else if(product === "Апельсин"){
         img.src = "img/apelsun.jpg";
+        cell.textContent = "27 рублей";
+        xcell = 27;
     }else if(product === "Ананас"){
         img.src = "img/ananas.jpg";
+        cell.textContent = "60 рублей";
+        xcell = 60;
     }else if(product === "Яблоки"){
         img.src = "img/yabloki.jpg";
+        cell.textContent = "44 рублей";
+        xcell = 44;
     }else if(product === "Виноград"){
         img.src = "img/inograd.jpg";
+        cell.textContent = "40 рублей";
+        xcell = 40;
     }else if(product === "Лимон"){
         img.src = "img/limone.jpg";
+        cell.textContent = "10 рублей";
+        xcell = 10;
     }
 });
 
 btn.addEventListener("click", function() {
     if(inp_coll.value != ""){
-        var chek = confirm("Вы уверены в заказе? " + inp_coll.value + " " + prod.value)
-        if(chek){
-            alert(inp_coll.value + " " + prod.value + " успешно заказаны")
-        }
-        
+        var chek = confirm("Вы уверены в заказе? ")
     }
 });
+
+add.addEventListener("click",function(event){
+    if(inp_coll.value != ""){
+        sum = xcell * inp_coll.value + sum;
+    }
+    cell_text.textContent = "Цена : " + sum +" Руб.";
+    event.preventDefault();
+})
+
+clear.addEventListener("click",function(event){
+    cell_text.textContent = "Цена : 0 Руб.";
+    sum = 0;
+    inp_coll.value = "";
+    event.preventDefault();
+})
